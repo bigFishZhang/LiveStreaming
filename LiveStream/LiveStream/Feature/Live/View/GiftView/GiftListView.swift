ab7cc53d27b58b1426ee7bffca33a83709099d8e
zzbbigfish <zzbbigfish@gmail.com>
2019-05-16 01:57:51 +0800

    基础文件

---
+++ LiveStream/LiveStream/Feature/Live/View/GiftView/GiftListView.swift
@@ -0,0 +1,14 @@
+//
+//  GiftListView.swift
+//  LiveStream
+//
+//  Created by zhang zhengbin on 2019/5/15.
+//  Copyright © 2019 zzb. All rights reserved.
+//
+
+import UIKit
+
+class GiftListView: UIView {
+
+
+}
---
+++ LiveStream/LiveStream/Feature/Live/View/GiftView/GiftViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GiftViewCell.swift
+//  LiveStream
+//
+//  Created by zhang zhengbin on 2019/5/15.
+//  Copyright © 2019 zzb. All rights reserved.
+//
+
+import UIKit
+
+class GiftViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
