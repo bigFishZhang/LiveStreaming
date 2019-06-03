9314491e75d956fd30a13b56a63c58a5fec19099
zzbbigfish <zzbbigfish@gmail.com>
2019-06-03 15:27:15 +0800

    【FIX】修改一下排版

--- LiveStream/LiveStream/Feature/Home/Other/ZBWaterFlowLayout.swift
+++ LiveStream/LiveStream/Feature/Home/Other/ZBWaterFlowLayout.swift
@@ -26,8 +26,11 @@ class ZBWaterFlowLayout: UICollectionViewFlowLayout {
     fileprivate var totalHeight : CGFloat = 0
     
     fileprivate lazy var colHeights : [CGFloat] = {
+        
         let cols = self.dataSource?.numberOfColsInWaterfallLayout?(self) ?? 2
+        
         var colHeights = Array(repeating: self.sectionInset.top, count: cols)
+        
         return colHeights
     }()
     
@@ -68,19 +71,27 @@ extension ZBWaterFlowLayout {
             }
             
             // 4.取出最小列的位置
-            var minH = colHeights.min()!
+            var minH = colHeights.min()! // sectionInset.top
+            
             let index = colHeights.firstIndex(of: minH)!
+            
+            let cellX:CGFloat = self.sectionInset.left + (self.minimumInteritemSpacing + itemW) * CGFloat(index)
+            
+            let cellY:CGFloat = minH - height - self.minimumLineSpacing
+            
+            // 5.设置item的属性
+            attrs.frame = CGRect(x:cellX , y:cellY , width: itemW, height: height)
+            
+            attrsArray.append(attrs)
+            
             minH = minH + height + minimumLineSpacing
             colHeights[index] = minH
             
-            // 5.设置item的属性
-            attrs.frame = CGRect(x: self.sectionInset.left + (self.minimumInteritemSpacing + itemW) * CGFloat(index), y: minH - height - self.minimumLineSpacing, width: itemW, height: height)
-            attrsArray.append(attrs)
         }
         // 4.记录最大值
         maxH = colHeights.max()!
         
-        // 5.给startIndex重新复制
+        // 5.给startIndex重新赋值
         startIndex = itemCount
      
     }
